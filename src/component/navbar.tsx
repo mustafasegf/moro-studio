8863a49a770b0f54cb3303ec629d90922b655188
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-03-14 21:14:49 +0700

    feat: added navbar and login

---
+++ src/component/navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link href="/" className="btn-ghost btn text-xl normal-case">
+            daisyUI
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export { Navbar };
