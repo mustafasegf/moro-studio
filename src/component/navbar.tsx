cf52114dd1d5129e72efb52b23e36d025695f4b2
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2023-03-21 11:20:37 +0700

    chore: navbar add edit homepage

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -5,22 +5,48 @@ function Navbar() {
     <>
       <div className="navbar bg-[#595959]">
         <div className="flex-1">
-          <Link href="/" className="btn-ghost btn text-white text-xl normal-case  hover:bg-gray-200 hover:text-gray-800">
+          <Link
+            href="/"
+            className="btn-ghost btn text-xl normal-case text-white  hover:bg-gray-200 hover:text-gray-800"
+          >
             Moro Studio
           </Link>
           <ul className="menu menu-horizontal px-1">
             <li>
-              <Link href="/listfeedback" className="mr-2 text-white hover:bg-gray-200 hover:text-gray-800">Feedback</Link>
+              <Link
+                href="/listfeedback"
+                className="mr-2 text-white hover:bg-gray-200 hover:text-gray-800"
+              >
+                Feedback
+              </Link>
             </li>
             <li>
-              <Link href="/list-catalogue" className="mr-2 text-white hover:bg-gray-200 hover:text-gray-800">Catalogue</Link>
+              <Link
+                href="/list-catalogue"
+                className="mr-2 text-white hover:bg-gray-200 hover:text-gray-800"
+              >
+                Catalogue
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/homepage/edit"
+                className="mr-2 text-white hover:bg-gray-200 hover:text-gray-800"
+              >
+                Edit Homepage
+              </Link>
             </li>
           </ul>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <Link href="/login" className="mr-2 text-white hover:bg-gray-200 hover:text-gray-800">Login</Link>
+              <Link
+                href="/login"
+                className="mr-2 text-white hover:bg-gray-200 hover:text-gray-800"
+              >
+                Login
+              </Link>
             </li>
           </ul>
         </div>
