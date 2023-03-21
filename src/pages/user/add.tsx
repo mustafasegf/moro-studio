402c0e5d3f7a5a6b9716542f68e5207c48f9d679
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-03-21 15:35:55 +0700

    feat: change ui for user add

--- src/pages/user/add.tsx
+++ src/pages/user/add.tsx
@@ -24,6 +24,14 @@ export default function AddUser() {
     }
   }, [addUser.isSuccess])
 
+  useEffect(function(){
+    if (addUser.isError) {
+      setInterval(() => {
+        addUser.reset()
+      }, 5000)
+    }
+  }, [addUser.isError])
+
   function handleSubmit(e: FormEvent) { 
     e.preventDefault()
     addUser.mutate(user)
@@ -40,6 +48,16 @@ export default function AddUser() {
   return (
     <>
 
+      {addUser.isError && (
+        <div className="toast">
+          <div className="alert alert-error">
+            <div>
+              <span>Error: {addUser.error.message}</span>
+            </div>
+          </div>
+        </div>
+      )}
+
       {addUser.isSuccess && (
         <div className="toast">
           <div className="alert alert-info">
@@ -50,86 +68,88 @@ export default function AddUser() {
         </div>
       )}
 
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <div className="">
-              <label
-                htmlFor="nama"
-                className="block text-sm font-medium leading-6"
-              >
-                Nama
-              </label>
-              <input
-                required
-                value={user.nama}
-                type="text"
-                id="nama"
-                className=""
-                onChange={(e) => setUser({ ...user, nama: e.target.value })}
-              />
-            </div>
-
-            <div className="">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6"
-              >
-                Email
-              </label>
-              <input
-                value={user.email}
-                required
-                type="text"
-                id="email"
-                className=""
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-              />
-            </div>
-
-            <div className="">
-              <label
-                htmlFor="hp"
-                className="block text-sm font-medium leading-6"
-              >
-                Hp
-              </label>
-              <input
-                value={user.hp}
-                type="text"
-                id="hp"
-                className=""
-                onChange={(e) => setUser({ ...user, hp: e.target.value })}
-              />
-            </div>
-
-            <div className="">
-              <label
-                htmlFor="role"
-                className="block text-sm font-medium leading-6"
-              >
-                Role
-              </label>
-              <select
-                value={user.role}
-                required
-                id="role"
-                className=""
-                // @ts-ignore
-                onChange={(e) => setUser({ ...user, role: e.target.value })}
-              >
-                {["admin", "studioManager", "blogManager", "user"].map(
-                  (role) => (
-                    <option>{role}</option>
-                  )
-                )}
-              </select>
-            </div>
+      <div className="m-8">
+        <div className="flex justify-center">
+          <form className="bg-base-200 p-8 rounded-3xl max-w-lg grow" onSubmit={handleSubmit}>
             <div>
-              <input className="btn mt-4" type="submit" />
+              <div className="">
+                <label
+                  htmlFor="nama"
+                  className="block text-sm font-medium leading-6"
+                >
+                  Nama
+                </label>
+                <input
+                  required
+                  value={user.nama}
+                  type="text"
+                  id="nama"
+                  className="input w-full max-w-md mt-1 mb-4 input-bordered"
+                  onChange={(e) => setUser({ ...user, nama: e.target.value })}
+                />
+              </div>
+
+              <div className="">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6"
+                >
+                  Email
+                </label>
+                <input
+                  value={user.email}
+                  required
+                  type="text"
+                  id="email"
+                  className="input w-full max-w-md mt-1 mb-4 input-bordered"
+                  onChange={(e) => setUser({ ...user, email: e.target.value })}
+                />
+              </div>
+
+              <div className="">
+                <label
+                  htmlFor="hp"
+                  className="block text-sm font-medium leading-6"
+                >
+                  Hp
+                </label>
+                <input
+                  value={user.hp}
+                  type="text"
+                  id="hp"
+                  className="input w-full max-w-md mt-1 mb-4 input-bordered"
+                  onChange={(e) => setUser({ ...user, hp: e.target.value })}
+                />
+              </div>
+
+              <div className="">
+                <label
+                  htmlFor="role"
+                  className="block text-sm font-medium leading-6"
+                >
+                  Role
+                </label>
+                <select
+                  value={user.role}
+                  required
+                  id="role"
+                  className="input w-full max-w-xs mt-1 mb-4 input-bordered"
+                  // @ts-ignore
+                  onChange={(e) => setUser({ ...user, role: e.target.value })}
+                >
+                  {["admin", "studioManager", "blogManager", "user"].map(
+                    (role) => (
+                      <option>{role}</option>
+                    )
+                  )}
+                </select>
+              </div>
+              <div>
+                <input className="btn mt-4" type="submit" />
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </>
   );
