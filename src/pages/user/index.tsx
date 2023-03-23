688a36f92aeaf6ce08564b86142be8d2bc169286
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-03-23 21:44:54 +0700

    feat: added generic modal action component

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,7 +1,7 @@
 import { User } from "@prisma/client";
 import { useState } from "react";
 import { api } from "~/utils/api";
-import { Modal } from "~/component/modal"
+import { Modal, ModalAction } from "~/component/modal"
 import Link from "next/link";
 import { GetServerSidePropsContext } from "next";
 import { getServerAuthSession } from "~/utils/session";
@@ -19,7 +19,6 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
 
 export default function ListUser() {
   const { data } = api.user.getAllUser.useQuery();
-
   const [user, setUser] = useState<User | null>(null)
   const [open, setOpen] = useState(false);
   const utils = api.useContext();
@@ -44,40 +43,35 @@ export default function ListUser() {
   }
 
   return <>
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <div className="z-50 overflow-y-auto">
-          <div className="flex items-center py-3 px-4">
-              <h3 className="text-lg font-medium">Delete User</h3>
-          </div>
-
-          <p className="py-4 px-4">
-            Apakah Anda yakin akan mengdelete user ini?
-          </p>
-          <div className="flex justify-end py-3 px-4">
-            <button
-              className="btn mr-4"
-              onClick={() => setOpen(false)}
-            >
-              Batal
-            </button>
-            <button
-              className="btn btn-error"
-              onClick={deleteHandler}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      </Modal>
+      <ModalAction
+        isDelete
+        open={open}
+        title="Delete User"
+        content="Apakah Anda yakin akan mengdelete user ini?"
+        onClose={() => setOpen(false)}
+        kembaliHandler={() => setOpen(false)}
+        actionHandler={deleteHandler}
+      />
 
     <div className="m-8">
       <div className="flex flex-row-reverse">
-        <Link href="/user/add" className="my-4 btn">
-          Add Account +
-        </Link>
+        <Link href="/user/add" className="my-4 btn">Add Account +</Link>
       </div>
       <div className="overflow-x-auto">
-        <table className="table table-zebra w-full">
+        <TableUser users={data} openModal={openModal} />
+      </div>
+    </div>
+  </>
+}
+
+interface UserTableProps {
+  users?: User[]
+  openModal: (user: User) => void
+}
+
+function TableUser({users, openModal}: UserTableProps) {
+  return <>
+    <table className="table table-zebra w-full">
           {/* head */}
           <thead>
             <tr>
@@ -91,7 +85,7 @@ export default function ListUser() {
             </tr>
           </thead>
           <tbody>
-            {data?.map((item, i) => (
+            {users?.map((item, i) => (
               <tr key={item.id}>
                 <th>{i+1}</th>
                 <td>{item.nama}</td>
@@ -100,13 +94,11 @@ export default function ListUser() {
                 <td>{item?.instagram ?? ""}</td>
                 <td>{item.role}</td>
                 <td> 
-                  <button onClick={() => openModal(item)} className="btn btn-error">Delele</button>
+                  <button onClick={() => openModal(item)} className="btn btn-error">Hapus</button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
-      </div>
-    </div>
   </>
 }
