a1d7ed92c72cdeb39e67bb7e854ec25845c33224
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-03-31 17:21:30 +0700

    feat: added pagination

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,49 +1,80 @@
 import { User } from "@prisma/client";
 import { useState } from "react";
 import { api } from "~/utils/api";
-import { Modal, ModalAction } from "~/component/modal"
+import { ModalAction } from "~/component/modal";
 import Link from "next/link";
 import { GetServerSidePropsContext } from "next";
 import { getServerAuthSession } from "~/utils/session";
-import { LoadingPage, LoadingSpiner } from "~/component/loading";
+import { LoadingPage } from "~/component/loading";
+import { useRouter } from "next/router";
+import { z } from "zod";
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-  const session = getServerAuthSession(ctx)
+  const session = getServerAuthSession(ctx);
   if (!session) {
     return { redirect: { destination: "/login" } };
   }
   if (session.role !== "admin") {
     return { redirect: { destination: "/" } };
   }
-  return {props: {}}
+  return { props: {} };
 }
 
 export default function ListUser() {
-  const { data, isLoading } = api.user.getAllUser.useQuery();
-  const [user, setUser] = useState<User | null>(null)
+  const router = useRouter();
+  const { page: pageQuery } = router.query;
+  const parsedPageQuery = z.preprocess(
+    (val) => parseInt(val as string, 10),
+    z.number().min(1)
+  ).safeParse(pageQuery); 
+
+  const initialPage = parsedPageQuery.success ? parsedPageQuery.data : 1;
+  const [page, setPage] = useState(initialPage);
+
+  const { data, isLoading } = api.user.getAllUser.useQuery({ page });
+
+  const users = data?.items 
+  const hasNextPage = data?.hasNextPage;
+
+  function fetchNextPage() {
+    setPage((prev) => prev + 1);
+    router.replace({
+      query: { page: page + 1 }
+    })
+  }
+
+  function fetchPrevPage() {
+    setPage((prev) => prev - 1);
+    router.replace({
+      query: { page: page - 1 }
+    })
+  }
+
+  const [user, setUser] = useState<User | null>(null);
   const [open, setOpen] = useState(false);
   const utils = api.useContext();
 
   const deleteUser = api.user.deleteUser.useMutation({
     onSuccess() {
-      void utils.user.getAllUser.invalidate()
-    }
-  })
+      void utils.user.getAllUser.invalidate();
+    },
+  });
 
   function openModal(user: User) {
-    setUser(user)
-    setOpen(true)
+    setUser(user);
+    setOpen(true);
   }
 
   function deleteHandler() {
     if (user) {
-      deleteUser.mutate(user)
+      deleteUser.mutate(user);
     }
 
-    setOpen(false)
+    setOpen(false);
   }
 
-  return <>
+  return (
+    <>
       <ModalAction
         isDelete
         open={open}
@@ -54,55 +85,81 @@ export default function ListUser() {
         actionHandler={deleteHandler}
       />
 
-    <div className="m-8">
-      <div className="flex flex-row-reverse">
-        <Link href="/user/add" className="my-4 btn">Add Account +</Link>
+      <div className="m-8">
+        <div className="flex flex-row-reverse">
+          <Link href="/user/add" className="btn my-4">
+            Add Account +
+          </Link>
+        </div>
+        <div className="overflow-x-auto">
+          {isLoading ? (
+            <LoadingPage />
+          ) : (
+            <> 
+              <TableUser users={users} openModal={openModal} />
+                <div className="flex items-center justify-center gap-4 my-4">
+                  {page > 1 && (
+                    <button onClick={() => fetchPrevPage()} className="btn">
+                      prev
+                    </button>
+                  )}
+                  halaman {page}
+                  {hasNextPage && (
+                    <button onClick={() => fetchNextPage()} className="btn">
+                      next
+                    </button>
+                  )}
+
+                </div>
+            </>
+          )}
+        </div>
       </div>
-      <div className="overflow-x-auto">
-        {isLoading 
-          ? <LoadingPage />
-          : <TableUser users={data} openModal={openModal} />
-        }
-      </div>
-    </div>
-  </>
+    </>
+  );
 }
 
-interface UserTableProps {
-  users?: User[]
-  openModal: (user: User) => void
+interface TableUserProps {
+  users?: User[];
+  openModal: (user: User) => void;
 }
 
-function TableUser({users, openModal}: UserTableProps) {
-  return <>
-    <table className="table table-zebra w-full">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Nama</th>
-              <th>Email</th>
-              <th>No hp</th>
-              <th>instagram</th>
-              <th>Role</th>
-              <th></th>
+function TableUser({ users, openModal }: TableUserProps) {
+  return (
+    <>
+      <table className="table-zebra table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>No hp</th>
+            <th>instagram</th>
+            <th>Role</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((item, i) => (
+            <tr key={item.id}>
+              <th>{i + 1}</th>
+              <td>{item.nama}</td>
+              <td>{item.email}</td>
+              <td>{item.hp}</td>
+              <td>{item?.instagram ?? ""}</td>
+              <td>{item.role}</td>
+              <td>
+                <button
+                  onClick={() => openModal(item)}
+                  className="btn-error btn"
+                >
+                  Hapus
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {users?.map((item, i) => (
-              <tr key={item.id}>
-                <th>{i+1}</th>
-                <td>{item.nama}</td>
-                <td>{item.email}</td>
-                <td>{item.hp}</td>
-                <td>{item?.instagram ?? ""}</td>
-                <td>{item.role}</td>
-                <td> 
-                  <button onClick={() => openModal(item)} className="btn btn-error">Hapus</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-  </>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,34 +1,36 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  adminProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, adminProcedure } from "~/server/api/trpc";
 import { addUserSchema } from "~/utils/schemas";
 
 export const userRouter = createTRPCRouter({
-  addUser: adminProcedure.input(addUserSchema).mutation(async ({ input, ctx }) => {
-    const user = await ctx.prisma.user.findFirst({
-      where: {
-        email: input.email,
-        deleted: false,
+  addUser: adminProcedure
+    .input(addUserSchema)
+    .mutation(async ({ input, ctx }) => {
+      const user = await ctx.prisma.user.findFirst({
+        where: {
+          email: input.email,
+          deleted: false,
+        },
+      });
+
+      if (user) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Email sudah terdaftar",
+        });
       }
-    })
 
-    if (user) {
-      throw new TRPCError({code: "BAD_REQUEST", message: "Email sudah terdaftar"})
-    }
-
-    return ctx.prisma.user.create({
-      data: {
-        email: input.email,
-        nama: input.nama,
-        hp: input.hp || undefined,
-        role: input.role,
-      },
-    });
-  }),
+      return ctx.prisma.user.create({
+        data: {
+          email: input.email,
+          nama: input.nama,
+          hp: input.hp || undefined,
+          role: input.role,
+        },
+      });
+    }),
 
   deleteUser: adminProcedure
     .input(
@@ -44,11 +46,37 @@ export const userRouter = createTRPCRouter({
       });
     }),
 
-  getAllUser: adminProcedure.query(async ({ ctx }) => {
-    return ctx.prisma.user.findMany({
-      where: {
-        deleted: false,
-      },
-    });
-  }),
+  getAllUser: adminProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        page: z.number().nullish(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const limit = input.limit ?? 100;
+      const { page } = input;
+
+      const items = await ctx.prisma.user.findMany({
+        take: limit + 1,
+        skip: page ? (page - 1) * limit : 0,
+        where: {
+          deleted: false,
+        },
+        orderBy: {
+          id: "desc",
+        },
+      });
+
+      let hasNextPage = false
+      if (items.length > limit) {
+        hasNextPage = true
+        items.pop()
+      }
+
+      return {
+        items,
+        hasNextPage,
+      };
+    }),
 });
