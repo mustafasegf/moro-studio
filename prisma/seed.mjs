83eae1f0603a904c52cc15d7782350ceecccb353
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-05-22 10:37:01 +0700

    feat: added seed db for background foto

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -2,55 +2,51 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const admin = await prisma.user.upsert({
-    where: {
-      id: "clfks2ea00000tbl1qakulgm3",
-    },
-    update: {},
-    create: {
+  const userData = [
+    {
       id: "clfks2ea00000tbl1qakulgm3",
       email: "admin@example.com",
       nama: "Admin",
       role: "admin",
     },
-  });
-
-  const blogManager = await prisma.user.upsert({
-    where: {
-      id: "clfksezx30004tbl1lm78ybny",
-    },
-    update: {},
-    create: {
+    {
       id: "clfksezx30004tbl1lm78ybny",
       email: "blogmanager@example.com",
       nama: "Blog Manager",
       role: "blogManager",
     },
-  });
-
-  const studioManager = await prisma.user.upsert({
-    where: { id: "clfkt21s3000atbl1inzhynab" },
-    update: {},
-    create: {
+    {
       id: "clfkt21s3000atbl1inzhynab",
       email: "studiomanager@example.com",
       nama: "Studio Manager",
       role: "studioManager",
     },
-  });
-
-  const user = await prisma.user.upsert({
-    where: {
-      id: "clfks6eab0002tbl1vzmqzv9z",
-    },
-    update: {},
-    create: {
+    {
       id: "clfks6eab0002tbl1vzmqzv9z",
       email: "user@example.com",
       nama: "User",
       role: "user",
     },
-  });
+  ];
+
+  const user = [];
+  for (const data of userData) {
+    user.push(
+      await prisma.user.upsert({
+        where: {
+          id: data.id,
+        },
+        update: {},
+        create: {
+          id: data.id,
+          email: data.email,
+          nama: data.nama,
+          // @ts-ignore
+          role: data.role,
+        },
+      })
+    );
+  }
 
   const katalogData = [
     {
@@ -113,15 +109,15 @@ All digital Soft files`,
       nama: "Group of 7 Self Photo",
       durasi: 80,
       harga: 350_000,
-      deskripsi: `Additional Person @30.000 (include 1 printed photo each)
-40 Minutes Photoshoot
+      deskripsi: `40 Minutes Photoshoot
 20 Minutes Photo Selection
 Various Properties and Frames
-All digital Soft files`,
+All digital Soft files
+Additional Person @30.000 (include 1 printed photo each)`,
     },
   ];
 
-  const katalog = []
+  const katalog = [];
   for (const data of katalogData) {
     katalog.push(
       await prisma.katalog.upsert({
@@ -131,21 +127,44 @@ All digital Soft files`,
         update: {},
         create: data,
       })
-    )
+    );
   }
 
-  const backgroundFoto = await prisma.backgroundFoto.upsert({
-    where: {
-      warna: "hitam"
+  const backgroundFotoData = [
+    {
+      warna: "White Background (Preset Colorfull)",
     },
-    update: {},
-    create: {
-      warna: "hitam",
+    {
+      warna: "White Background (Preset Black & White)",
     },
+    {
+      warna: "Black Background (Preset Colorfull)",
+    },
+    {
+      warna: "Black Background (Preset Black & White)",
+    },
+  ];
+
+  const backgroundFoto = [];
+  for (const data of backgroundFotoData) {
+    backgroundFoto.push(
+      await prisma.backgroundFoto.upsert({
+        where: {
+          warna: data.warna,
+        },
+        update: {},
+        create: {
+          warna: data.warna,
+        },
+      })
+    );
+  }
+
+  console.log({
+    user,
+    katalog,
+    backgroundFoto,
   });
-
-
-  console.log({ admin, blogManager, studioManager, user, katalog, backgroundFoto });
 }
 
 main()
