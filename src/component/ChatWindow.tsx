dfe7745aaa4f0837231a19ca616e4f56343e6851
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2023-06-04 18:38:23 +0700

    create chat component

---
+++ src/component/ChatWindow.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ChatBubble from "./ChatBubble";
+
+interface ChatWindowProps {
+  messages: { sender: string; message: string; isOwnMessage: boolean }[];
+}
+
+const ChatWindow: React.FC<ChatWindowProps> = ({ messages }) => {
+  return (
+    <div className="chat-window">
+      {messages.map((msg, index) => (
+        <ChatBubble
+          key={index}
+          sender={msg.sender}
+          message={msg.message}
+          isOwnMessage={msg.isOwnMessage}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ChatWindow;
