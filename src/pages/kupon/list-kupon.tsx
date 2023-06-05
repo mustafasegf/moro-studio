8dcd00723e186303bc5ba21f62a0e7dd16d2fc4e
Regina Febrian Pehry <regina.febrian@ui.ac.id>
2023-06-05 20:29:55 +0700

    perbaiki tampilan v2

--- src/pages/kupon/list-kupon.tsx
+++ src/pages/kupon/list-kupon.tsx
@@ -72,59 +72,134 @@ export default function ListKupon() {
         kembaliHandler={() => setOpen(false)}
         actionHandler={deleteHandler}
       />
+      <div className="min-h-screen">
+        <h1 className="my-8 text-center text-3xl font-bold">Daftar Kupon</h1>
 
-      <h1 className="my-8 text-center text-3xl font-bold">Daftar Kupon</h1>
+        <div className="mb-4 flex justify-end">
+          <Link
+            href="/kupon/create-kupon"
+            className="mr-5 rounded-md bg-blue px-6 py-2 text-white-grey hover:bg-[#6380BB]"
+          >
+            + Buat Kupon
+          </Link>
+        </div>
 
-      <div className="mb-4 flex justify-end">
-        <Link
-          href="/kupon/create-kupon"
-          className="mr-5 rounded-md bg-blue px-6 py-2 text-white-grey hover:bg-[#6380BB]"
-        >
-          + Buat Kupon
-        </Link>
-      </div>
-
-      <div className="m-5 overflow-x-auto relative z-0">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <td className="bg-light-grey bg-opacity-80 text-base normal-case"></td>
-              <th className="bg-light-grey bg-opacity-80 text-base normal-case">Nama Kupon</th>
-              <th className="bg-light-grey bg-opacity-80 text-base normal-case">Kode</th>
-              <th className="bg-light-grey bg-opacity-80 text-base normal-case">Diskon</th>
-              <th className="bg-light-grey bg-opacity-80 text-base normal-case">Batas Pemakaian</th>
-              <th className="bg-light-grey bg-opacity-80 text-base normal-case">Digunakan</th>
-              <th className="bg-light-grey bg-opacity-80 text-base normal-case">Status</th>
-              <th className="bg-light-grey bg-opacity-80 text-base normal-case">Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.map((item, i) => (
-              <tr key={item.id}>
-                <td className={i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}>{i + 1}</td>
-                <td className={i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}>{item.nama}</td>
-                <td className={i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}>{item.kode}</td>
-                <td className={i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}>{item.diskon}%</td>
-                <td className={i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}>{item.kuotaPemakaian}</td>
-                <td className={i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}>{item.kuotaTerpakai}</td>
-                <td className={i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}>
-                  {new Date(item.tanggal) >=
-                  new Date(new Date().setHours(0, 0, 0, 0))
-                    ? "Sedang Berjalan"
-                    : "Telah Berakhir"}
-                </td>
-                <td className={`flex items-center ${i % 2 === 0 ? 'bg-white-grey bg-opacity-50' : 'bg-light-grey bg-opacity-40'}`}>
-                  <Link href={`/kupon/ubah/${item.id}`}>
-                    <BiEdit className="mr-2 text-2xl" />
-                  </Link>
-                  <button onClick={() => handleDeleteButton(item)}>
-                    <MdDelete className="text-2xl" />
-                  </button>
-                </td>
+        <div className="relative z-0 m-5 overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <td className="bg-light-grey bg-opacity-80 text-base normal-case"></td>
+                <th className="bg-light-grey bg-opacity-80 text-base normal-case">
+                  Nama Kupon
+                </th>
+                <th className="bg-light-grey bg-opacity-80 text-base normal-case">
+                  Kode
+                </th>
+                <th className="bg-light-grey bg-opacity-80 text-base normal-case">
+                  Diskon
+                </th>
+                <th className="bg-light-grey bg-opacity-80 text-base normal-case">
+                  Batas Pemakaian
+                </th>
+                <th className="bg-light-grey bg-opacity-80 text-base normal-case">
+                  Digunakan
+                </th>
+                <th className="bg-light-grey bg-opacity-80 text-base normal-case">
+                  Status
+                </th>
+                <th className="bg-light-grey bg-opacity-80 text-base normal-case">
+                  Aksi
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data?.map((item, i) => (
+                <tr key={item.id}>
+                  <td
+                    className={
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }
+                  >
+                    {i + 1}
+                  </td>
+                  <td
+                    className={
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }
+                  >
+                    {item.nama}
+                  </td>
+                  <td
+                    className={
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }
+                  >
+                    {item.kode}
+                  </td>
+                  <td
+                    className={
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }
+                  >
+                    {item.diskon}%
+                  </td>
+                  <td
+                    className={
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }
+                  >
+                    {item.kuotaPemakaian}
+                  </td>
+                  <td
+                    className={
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }
+                  >
+                    {item.kuotaTerpakai}
+                  </td>
+                  <td
+                    className={
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }
+                  >
+                    {new Date(item.tanggal) >=
+                    new Date(new Date().setHours(0, 0, 0, 0))
+                      ? "Sedang Berjalan"
+                      : "Telah Berakhir"}
+                  </td>
+                  <td
+                    className={`flex items-center ${
+                      i % 2 === 0
+                        ? "bg-white-grey bg-opacity-50"
+                        : "bg-light-grey bg-opacity-40"
+                    }`}
+                  >
+                    <Link href={`/kupon/ubah/${item.id}`}>
+                      <BiEdit className="mr-2 text-2xl" />
+                    </Link>
+                    <button onClick={() => handleDeleteButton(item)}>
+                      <MdDelete className="text-2xl" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
